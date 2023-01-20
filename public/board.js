c451f0aaed81955e3451d25b5f7c2203dc7e774d
Anthony <anthonymenardgill@gmail.com>
2023-01-19 21:16:12 -0500

    changed settings for aws ec2 hosting

--- public/board.js
+++ public/board.js
@@ -1,80 +1,78 @@
 export let sketch = (p) => {
-    p5.disableFriendlyErrors = true;
-    p.socket = io('https://shareablewhiteboard.herokuapp.com/', {
-        reconnection: false,
-        transports: ['websocket'],
-        upgrade: false
-    });
+  p5.disableFriendlyErrors = true;
+  //https://shareablewhiteboard.herokuapp.com/
+  p.socket = io("http://54.165.7.78:3100", {
+    reconnection: false,
+    transports: ["websocket"],
+    upgrade: false,
+  });
 
-    // p.socket = io("192.168.0.195:5000", {
-    //     reconnection: false,
-    //     transports: ['websocket'],
-    //     upgrade: false
-    // });
-    p.color;
-    p.strokeW;
-    p.erasing = false;
-    p.panning = false;
-    p.scaleFactor = 1;
-    p.translateX = 0;
-    p.translateY = 0;
-    p.topLeftAndBottomRightCorners = [];
+  // p.socket = io("192.168.0.195:5000", {
+  //     reconnection: false,
+  //     transports: ['websocket'],
+  //     upgrade: false
+  // });
+  p.color;
+  p.strokeW;
+  p.erasing = false;
+  p.panning = false;
+  p.scaleFactor = 1;
+  p.translateX = 0;
+  p.translateY = 0;
+  p.topLeftAndBottomRightCorners = [];
 
-    p.setup = () => {
-
-        /*========================================================================================================================
+  p.setup = () => {
+    /*========================================================================================================================
                                                         Sketch Setup
           ========================================================================================================================*/
-        p.colorPic = p.select('#colorPic');
-        p.strokeSlider = p.select('#sizeSlider');
-        p.canvas = p.createCanvas(p.windowWidth / 1.5, p.windowHeight / 1.5);
-        p.canvas.parent('wrap', '');
-        p.canvas.addClass("no-select")
-        p.background('#F');
-        p.topLeftAndBottomRightCorners = [[(p.translateX / p.scaleFactor), (p.translateY / p.scaleFactor)]
-            , [((p.width + p.translateX) / p.scaleFactor), ((p.translateY + p.height) / p.scaleFactor)]]
+    p.colorPic = p.select("#colorPic");
+    p.strokeSlider = p.select("#sizeSlider");
+    p.canvas = p.createCanvas(p.windowWidth / 1.5, p.windowHeight / 1.5);
+    p.canvas.parent("wrap", "");
+    p.canvas.addClass("no-select");
+    p.background("#F");
+    p.topLeftAndBottomRightCorners = [
+      [p.translateX / p.scaleFactor, p.translateY / p.scaleFactor],
+      [(p.width + p.translateX) / p.scaleFactor, (p.translateY + p.height) / p.scaleFactor],
+    ];
+  };
 
-    }
+  p.draw = () => {
+    p.translate(p.translateX, p.translateY);
+    p.scale(p.scaleFactor);
 
-    p.draw = () => {
-        p.translate(p.translateX, p.translateY);
-        p.scale(p.scaleFactor)
+    if (p.mouseX < p.width + 10 && p.mouseX > -10 && p.mouseY < p.height + 10 && p.mouseY > -10) {
+      if (!p.panning) {
+        if (p.erasing) p.c = p.color("#F");
+        else p.c = p.color(p.colorPic.value());
 
-        if (p.mouseX < p.width + 10 && p.mouseX > -10 && p.mouseY < p.height + 10 && p.mouseY > -10) {
-            if (!p.panning) {
-                if (p.erasing)
-                    p.c = p.color('#F');
-                else
-                    p.c = p.color(p.colorPic.value());
+        if (p.mouseIsPressed) {
+          let data = {
+            x: (p.mouseX - p.translateX) / p.scaleFactor,
+            y: (p.mouseY - p.translateY) / p.scaleFactor,
+            px: (p.pmouseX - p.translateX) / p.scaleFactor,
+            py: (p.pmouseY - p.translateY) / p.scaleFactor,
+            erasing: p.erasing,
+            incomingWidth: p.width,
+            incomingHeight: p.height,
+            incomingStroke: p.strokeW,
+            color: p.c,
+          };
 
-                if (p.mouseIsPressed) {
-                    let data = {
-                        x: (p.mouseX - p.translateX) / p.scaleFactor,
-                        y: (p.mouseY - p.translateY) / p.scaleFactor,
-                        px: (p.pmouseX - p.translateX) / p.scaleFactor,
-                        py: (p.pmouseY - p.translateY) / p.scaleFactor,
-                        erasing: p.erasing,
-                        incomingWidth: p.width,
-                        incomingHeight: p.height,
-                        incomingStroke: p.strokeW,
-                        color: p.c
-                    }
-
-                    p.socket.emit('someoneDrew', data);
-                    p.strokeW = p.strokeSlider.value()
-                    p.strokeWeight(p.strokeW);
-                    p.stroke(data.color);
-                    p.line(data.x, data.y, data.px, data.py);
-                }
-            } else {
-                p.select('#wrap').style('cursor: grab')
-                if (p.mouseIsPressed) {
-                    p.select('#wrap').style('cursor: move')
-                    let panningEvent = new CustomEvent('panning')
-                    document.querySelector('#wrap').dispatchEvent(panningEvent);
-                }
-            }
+          p.socket.emit("someoneDrew", data);
+          p.strokeW = p.strokeSlider.value();
+          p.strokeWeight(p.strokeW);
+          p.stroke(data.color);
+          p.line(data.x, data.y, data.px, data.py);
         }
+      } else {
+        p.select("#wrap").style("cursor: grab");
+        if (p.mouseIsPressed) {
+          p.select("#wrap").style("cursor: move");
+          let panningEvent = new CustomEvent("panning");
+          document.querySelector("#wrap").dispatchEvent(panningEvent);
+        }
+      }
     }
-}
-
+  };
+};
--- src/server.js
+++ src/server.js
@@ -1,69 +1,63 @@
-const express = require('express');
-const socket = require('socket.io');
-const mainRoutes = require('./routes/mainRoutes')
-const MessageBuilder = require('./MessageBuilder.js');
-const PORT = process.env.PORT || 5000;
+const express = require("express");
+const socket = require("socket.io");
+const mainRoutes = require("./routes/mainRoutes");
+const MessageBuilder = require("./MessageBuilder.js");
+const PORT = process.env.PORT || 3100;
 const app = express();
 let server = app.listen(PORT);
 let io = socket(server);
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(mainRoutes);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-let points = []
-let messages = []
+let points = [];
+let messages = [];
 let users = {};
 
-io.on('connection', (socket) => {
-    console.log('new connection: ' + socket.id);
+io.on("connection", (socket) => {
+  console.log("new connection: " + socket.id);
 
-    socket.on('disconnect', (reason) => {
-        console.log(`disconecting: ${socket.id}\n\t because: ${reason}`);
-        let systemMessage = MessageBuilder.formatServerMessage(`${users[socket.id]} has left.`);
-        delete users[socket.id];
-        messages.push(systemMessage);
-        io.emit('message', systemMessage);
-    })
-    socket.on('someoneDrew', (data) => {
-        points.push(data)
-        socket.broadcast.emit('someoneDrew', data);
-    })
+  socket.on("disconnect", (reason) => {
+    console.log(`disconecting: ${socket.id}\n\t because: ${reason}`);
+    let systemMessage = MessageBuilder.formatServerMessage(`${users[socket.id]} has left.`);
+    delete users[socket.id];
+    messages.push(systemMessage);
+    io.emit("message", systemMessage);
+  });
+  socket.on("someoneDrew", (data) => {
+    points.push(data);
+    socket.broadcast.emit("someoneDrew", data);
+  });
 
-    socket.on('load', (message) => {
-        io.to(socket.id).emit('load', points)
-    })
+  socket.on("load", (message) => {
+    io.to(socket.id).emit("load", points);
+  });
 
-    socket.on('message', (message) => {
-        formattedMessage = MessageBuilder.formatUserMessage(message.nickname, message.text);
-        messages.push(formattedMessage);
-        io.emit('message', formattedMessage);
-    })
+  socket.on("message", (message) => {
+    formattedMessage = MessageBuilder.formatUserMessage(message.nickname, message.text);
+    messages.push(formattedMessage);
+    io.emit("message", formattedMessage);
+  });
 
-    socket.on('loadMessages', () => {
-        io.to(socket.id).emit('loadMessages', messages);
-    })
+  socket.on("loadMessages", () => {
+    io.to(socket.id).emit("loadMessages", messages);
+  });
 
-    socket.on('connected', (nickname) => {
-        users[socket.id] = nickname;
-        let systemMessage = MessageBuilder.formatServerMessage(`${nickname} has joined! Welcome :^)`);
-        messages.push(systemMessage);
-        socket.broadcast.emit('message', systemMessage);
-        io.to(socket.id).emit('loadMessages', messages);
-    })
+  socket.on("connected", (nickname) => {
+    users[socket.id] = nickname;
+    let systemMessage = MessageBuilder.formatServerMessage(`${nickname} has joined! Welcome :^)`);
+    messages.push(systemMessage);
+    socket.broadcast.emit("message", systemMessage);
+    io.to(socket.id).emit("loadMessages", messages);
+  });
 
-    socket.on('disconnect', (reason) => {
+  socket.on("disconnect", (reason) => {});
 
-    })
-
-    io.to(socket.id).emit('load', points);
-})
-
-
-
-
-
-
+  io.to(socket.id).emit("load", points);
+});
