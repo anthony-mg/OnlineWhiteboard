121878966f1b73458298094871f460356e28d7a9
Anthony <anthonymenardgill@gmail.com>
2023-01-25 05:27:22 -0500

    url again..

--- public/board.js
+++ public/board.js
@@ -1,7 +1,7 @@
 export let sketch = (p) => {
   p5.disableFriendlyErrors = true;
   //https://shareablewhiteboard.herokuapp.com/
-  p.socket = io("http://54.165.7.78:5100", {
+  p.socket = io("http://54.165.7.78:3100", {
     reconnection: false,
     transports: ["websocket"],
     upgrade: false,
